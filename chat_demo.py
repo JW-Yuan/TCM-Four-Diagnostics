68aa3837522806c0c208396fdc85e5079b49ec80
Yuanjw <einstein99ium@gmail.com>
2025-03-10 16:47:05 +0800

    edit
    edit

--- chat_demo.py
+++ chat_demo.py
@@ -6,18 +6,20 @@ from chat_model.local_chat import LocalChat
 '''
 
 if __name__=="__main__":
-    question="你是谁？"
-    #1、使用本地大模型
+    question="你这个api支持图片的输入吗？"
+    # #1、使用云端大模型
+    api_key="sk-d69f89a753d74b399a9404194d611aaa"
+    base_url="https://api.deepseek.com"
+    CloudModel=CloudChat(api_key,base_url)
+    cloud_answer=CloudModel.get_answer(question)
+    print("*********************以下是云端大模型回答*******************")
+    print(cloud_answer)
+
+    # #2、利用ollama使用本地大模型
     LocalModel=LocalChat()
     think_text, answer_text = LocalModel.get_answer(question)
     print("*********************以下是本地大模型回答*******************")
     print("Think 部分：", think_text)
     print("Answer 部分：", answer_text)
-    #2、使用云端大模型
-    api_key="sk-d69f89a753d74b399a9404194d611aaa"
-    base_url="https://api.deepseek.com"
-    CloudModel=CloudChat(api_key,base_url)
-    cloud_answer=CloudModel.get_answer(question)
-    print("*********************以下是本地大模型回答*******************")
-    print(cloud_answer)
 
+    # #3、直接使用本地大模型（待加），预计使用ds-vl，蹭一下多模态
